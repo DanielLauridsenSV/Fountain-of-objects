9698f307b9edf5e752395800d38a774d3ebbe326
Daniel Lauridsen <daniel@pingas.se>
2021-10-25 13:03:51 +0200

    commit

--- Fountain of objects/supportclass.cs
+++ Fountain of objects/supportclass.cs
@@ -8,13 +8,13 @@ namespace Fountain_of_objects
         /// <summary>
         /// used instead of console.Readkey to make play more intuitive
         /// </summary>
-        public static void Sleep()
+        public static void Sleep( int time =230)
         {
             string dots = (".....");
             for (int i = 0; i < dots.Length; i++)
             {
                 Console.Write(dots[i]);
-                Thread.Sleep(230);
+                Thread.Sleep(3230);
             }
         }
     }
